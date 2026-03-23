72c61db10e1da853ce73ab78620ad303f8a3563b
Mahmut Can Yılmaz <mcyilmaz64@hotmail.com>
2026-03-24 01:16:03 +0300

    UoW And DI Classes added

---
+++ DomainDrivenDesign.Application/DependenccyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using System.Reflection;
+
+namespace DomainDrivenDesign.Application
+{
+    public static class DependenccyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+            return services;
+        }
+    }
+}
